ce62e86babe6fd7f97bda9f60db7baf2f67f04b6
Slepotek <mercim2@gmail.com>
2023-08-07 12:27:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.innerHTML = "Member";
+    member.style.color = "white";
+    member.style.backgroundColor = "black";
+    member.style.borderRadius = "5px";
+    member.style.padding = "5px";
+    member.style.margin = "5px";
+    member.style.textAlign = "center";
+    member.style.fontFamily = "Arial";
+    member.style.fontSize = "20px";
+    member.style.fontWeight = "bold";
+    member.style.width = "100px";
+    member.style.height = "50px";
+    member.style.lineHeight = "50px";
+    member.style.boxShadow = "0 0 10px black";
+    member.style.transition = "all 0.5s";
+}
